392abc9d4622430cdb1f33bd2b2a2d5033307cfc
climb719 <mary.oldweiler@gmail.com>
2021-01-25 12:09:56 -0500

    commit tree f3bfc31663901f87f1720168635a8880b3dd87d3

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram #create a class 
+    attr_accessor :word
+    #create a setter and getter method for the initialization method
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_array) 
+        #to deterimine if one word is an anagram for another: 
+        #determine if the words are compossed of the same letters. 
+        #iterate over each word that the .match method takes as an argument.
+        #compare each word of the array to the word that was initialized.
+        word_array.select do |word| 
+        word.split("").sort == @word.split("").sort
+        end  
+    end
+end
+
